03688723e1930196c470058c28dd7134b0cc1984
David GARDIE <74535282+Animus-Necandi@users.noreply.github.com>
2023-05-19 10:55:58 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-import https from 'https';
+const https = require('https');
 
 try {
   https.get('https://dechetgo.000webhostapp.com/', (res) => {
@@ -8,31 +8,74 @@ try {
     });
     res.on('end', () => {
       const dataArray = JSON.parse(data);
-      dataArray.forEach((element) => {
-        if (element.hasOwnProperty('idDecheterie')) {
-          console.log(element.idDecheterie);
+      const newData = JSON.stringify(dataArray);
+
+      const options = {
+        hostname: 'dechet-go-git-main-animus-necandi.vercel.app',
+        path: '/api/data',
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          'Content-Length': newData.length
         }
-        if (element.hasOwnProperty('nomDecheterie')) {
-          console.log(element.nomDecheterie);
-        }
-        if (element.hasOwnProperty('adresseDecheterie')) {
-          console.log(element.adresseDecheterie);
-        }
-        if (element.hasOwnProperty('telDecheterie')) {
-          console.log(element.telDecheterie);
-        }
-        if (element.hasOwnProperty('mailDecheterie')) {
-          console.log(element.mailDecheterie);
-        }
-        console.log("\n");
+      };
+
+      const req = https.request(options, (res) => {
+        res.on('data', (response) => {
+          console.log('Response:', response.toString());
+        });
       });
+
+      req.on('error', (error) => {
+        console.error('Error:', error.message);
+      });
+
+      req.write(newData);
+      req.end();
     });
   }).on('error', (err) => {
-    console.error('Error: ' + err.message);
+    console.error('Error:', err.message);
   });
 } catch (error) {
   console.error('Error:', error);
 }
+
+
+// import https from 'https';
+
+// try {
+//   https.get('https://dechetgo.000webhostapp.com/', (res) => {
+//     let data = '';
+//     res.on('data', (chunk) => {
+//       data += chunk;
+//     });
+//     res.on('end', () => {
+//       const dataArray = JSON.parse(data);
+//       dataArray.forEach((element) => {
+//         if (element.hasOwnProperty('idDecheterie')) {
+//           console.log(element.idDecheterie);
+//         }
+//         if (element.hasOwnProperty('nomDecheterie')) {
+//           console.log(element.nomDecheterie);
+//         }
+//         if (element.hasOwnProperty('adresseDecheterie')) {
+//           console.log(element.adresseDecheterie);
+//         }
+//         if (element.hasOwnProperty('telDecheterie')) {
+//           console.log(element.telDecheterie);
+//         }
+//         if (element.hasOwnProperty('mailDecheterie')) {
+//           console.log(element.mailDecheterie);
+//         }
+//         console.log("\n");
+//       });
+//     });
+//   }).on('error', (err) => {
+//     console.error('Error: ' + err.message);
+//   });
+// } catch (error) {
+//   console.error('Error:', error);
+// }
 
 
 
