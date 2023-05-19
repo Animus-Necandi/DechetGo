93618c2732ce2be9844ef955ba0318204a5aa177
David GARDIE <74535282+Animus-Necandi@users.noreply.github.com>
2023-05-19 11:52:31 +0200

    Update data.js

--- api/data.js
+++ api/data.js
@@ -1,44 +1,56 @@
-import https from 'https';
+import { fetch } from '@vercel/fetch';
 
-try {
-  https.get('https://dechetgo.000webhostapp.com/', (res) => {
-    let data = '';
-    res.on('data', (chunk) => {
-      data += chunk;
-    });
-    res.on('end', () => {
-      const dataArray = JSON.parse(data);
-      const newData = JSON.stringify(dataArray);
-
-      const options = {
-        hostname: 'dechet-go-git-main-animus-necandi.vercel.app',
-        path: '/',
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-          'Content-Length': newData.length
-        }
-      };
-
-      const req = https.request(options, (res) => {
-        res.on('data', (response) => {
-          console.log('Response:', response.toString());
-        });
-      });
-
-      req.on('error', (error) => {
-        console.error('Error:', error.message);
-      });
-
-      req.write(newData);
-      req.end();
-    });
-  }).on('error', (err) => {
-    console.error('Error:', err.message);
+export default async (req, res) => {
+  const fetchWithPermission = fetch(req, res, {
+    allow: ['https://dechetgo.000webhostapp.com/'] // Autorise uniquement les appels vers api.example.com
   });
-} catch (error) {
-  console.error('Error:', error);
-}
+
+  const response = await fetchWithPermission('hhttps://dechetgo.000webhostapp.com/');
+  const data = await response.json();
+
+  res.json(data);
+};
+// import https from 'https';
+
+// try {
+//   https.get('https://dechetgo.000webhostapp.com/', (res) => {
+//     let data = '';
+//     res.on('data', (chunk) => {
+//       data += chunk;
+//     });
+//     res.on('end', () => {
+//       const dataArray = JSON.parse(data);
+//       const newData = JSON.stringify(dataArray);
+
+//       const options = {
+//         hostname: 'dechet-go-git-main-animus-necandi.vercel.app',
+//         path: '/',
+//         method: 'POST',
+//         headers: {
+//           'Content-Type': 'application/json',
+//           'Content-Length': newData.length
+//         }
+//       };
+
+//       const req = https.request(options, (res) => {
+//         res.on('data', (response) => {
+//           console.log('Response:', response.toString());
+//         });
+//       });
+
+//       req.on('error', (error) => {
+//         console.error('Error:', error.message);
+//       });
+
+//       req.write(newData);
+//       req.end();
+//     });
+//   }).on('error', (err) => {
+//     console.error('Error:', err.message);
+//   });
+// } catch (error) {
+//   console.error('Error:', error);
+// }
 
 // const https = require('https');
 
